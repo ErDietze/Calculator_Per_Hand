86b535b85773c54e6006473da113bbc2129f2298
SenfDietze <SenfDietze@192.168.103.25>
2017-01-20 15:01:42 +0100

    insert lables und button

--- src/calculator_per_hand/Calculator_Per_Hand.java
+++ src/calculator_per_hand/Calculator_Per_Hand.java
@@ -8,6 +8,7 @@ package calculator_per_hand;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -22,12 +23,46 @@ import javafx.stage.Stage;
  */
 public class Calculator_Per_Hand extends Application {
 
-    TextField txtnu, txtnum2;
+    TextField txtnum1, txtnum2;
     Button btnadd, btnsub, btndiv, btnmul, btnclear;
     Label lblanswer;
 
     @Override
     public void start(Stage primaryStage) {
+        GridPane root = new GridPane();
+
+// make the controls
+        txtnum1 = new TextField();
+        txtnum2 = new TextField();
+        btnadd = new Button("+");
+        btnsub = new Button("-");
+        btnmul = new Button("*");
+        btndiv = new Button("+");
+        btnclear = new Button("Clear");
+        lblanswer = new Label("?");
+        // center text in label
+        lblanswer.setAlignment(Pos.CENTER);
+        //apply ccs-like style to label (yes, youcan);
+        lblanswer.setStyle("-fx-border-color: #000; fx-padding: ");
+        //put container 
+        root.setAlignment(Pos.CENTER);
+        //setspacing between controls in grid
+        root.setHgap(10);
+        root.setVgap(10);
+        //add to gris cell bycell
+        root.add(btnadd,0,0);
+        root.add(btnsub,1,0);
+        root.add(btnmul,0,1);
+        root.add(btndiv,1,1);
+        root.add(txtnum1,0,2);
+        root.add(txtnum2,1,2);
+        
+        //last 2 row´s span across 2 columns
+        //col,rol, colspan, rowspan
+        root.add(lblanswer,0,1,2,1);
+        root.add(lblanswer,0,4,2,1);
+        
+        
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -40,8 +75,6 @@ public class Calculator_Per_Hand extends Application {
 
 //        StackPane root = new StackPane();
 //        root.getChildren().add(btn);
-        GridPane root = new GridPane();
-
         Scene scene = new Scene(root, 300, 250);
 
         primaryStage.setTitle("Hello World!");
