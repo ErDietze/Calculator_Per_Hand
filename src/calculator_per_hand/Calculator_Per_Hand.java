32833b13555c8ed2b3f2badfeae35c5425dbe06c
SenfDietze <SenfDietze@192.168.103.25>
2017-01-20 14:32:52 +0100

    the begin to write the Code with jand and without Scene Builder

---
+++ src/calculator_per_hand/Calculator_Per_Hand.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator_per_hand;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author SenfDietze
+ */
+public class Calculator_Per_Hand extends Application {
+
+    TextField txtnu, txtnum2;
+    Button btnadd, btnsub, btndiv, btnmul, btnclear;
+    Label lblanswer;
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+        GridPane root = new GridPane();
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
